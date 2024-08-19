d256838c16f22813b775c135ebd171f6eb146e3e
Stefcio86 <mstefansiu@gmail.com>
2024-08-19 21:43:20 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+// src/App.jsx
+import React from 'react';
+import Alert from './Alert'; // Używamy default import
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="<https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640>"
-        price={14.29}
-      />
-    </div>
+    <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
   );
-}
+};
+
+export default App;
